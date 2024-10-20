94f649afc3832d0d0cedbb50f8adfded5f922604
Ian <adriangabrielramos010589@gmail.com>
2024-10-20 21:30:54 +0800

    Contact Page added

--- src/components/section/Contact/Contact.jsx
+++ src/components/section/Contact/Contact.jsx
@@ -1,10 +1,54 @@
 import "./Contact.css";
+import { images } from "./images";
+import { motion } from "framer-motion";
 
 export const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <h1 className="text-connect">Let's Connect</h1>
+        <motion.h1
+          className="text-connect"
+          initial={{ opacity: 0, y: -70 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            ease: "easeOut",
+            delay: 0.3,
+            duration: 0.5,
+          }}
+        >
+          Shoot Me a Message
+        </motion.h1>
+        <div className="box-container">
+          {images.map((image) => (
+            <div className="item-container" key={image.key}>
+              <motion.img
+                className="icon-contact"
+                src={image.imageSource}
+                alt="icon"
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  ease: "easeOut",
+                  delay: 0.5,
+                  duration: 0.5,
+                }}
+              />
+              <motion.div
+                className="text-container"
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  ease: "easeOut",
+                  delay: 0.6,
+                  duration: 0.5,
+                }}
+              >
+                <p className="placeholder">{image.placeholder}</p>
+                <p className="details">{image.name}</p>
+              </motion.div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/section/Contact/images.js
@@ -0,0 +1,34 @@
+const images = [
+  {
+    key: 1,
+    imageSource: "assets/mail.png",
+    placeholder: "Send me an email:",
+    name: "adriangabrielnramos@gmail.com",
+  },
+  {
+    key: 2,
+    imageSource: "assets/github.png",
+    placeholder: "Github account:",
+    name: "github.com/gabrielAdrianRamos",
+  },
+  {
+    key: 3,
+    imageSource: "assets/linkedin.png",
+    placeholder: "Linkedin account",
+    name: "linkedin.com/in/adrian-gabriel-ramos-b42329274",
+  },
+  {
+    key: 4,
+    imageSource: "assets/teepublic.png",
+    placeholder: "Teepublic store:",
+    name: "tee.pub/lic/Tobys",
+  },
+  {
+    key: 5,
+    imageSource: "assets/threadless.png",
+    placeholder: "Threadless store:",
+    name: "tobys.threadless.com",
+  },
+];
+
+export { images };
