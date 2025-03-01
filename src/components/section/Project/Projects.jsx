1eb198026dbf0eb13c89c2f90137d532b5c23eae
Ian <adriangabrielramos010589@gmail.com>
2025-03-01 18:58:11 +0800

    Added Card

--- src/components/section/Project/Projects.jsx
+++ src/components/section/Project/Projects.jsx
@@ -7,44 +7,64 @@ export const Projects = () => {
       <div className="project-container">
         <h1 className="project-name">Projects</h1>
         <div className="project-card">
-          <Card>
-            <div className="card-image-container">
-              <img
-                src="\assets\playground.png"
-                alt="title"
-                className="card-image"
-              />
-            </div>
-            <div className="card-text-container">
-              <p className="card-title">Toby's Playground</p>
-              <p className="description">
-                Toby's Playground is a fun and interactive web app designed for
-                toddlers.
-              </p>
-              <a href="https://playground-toby.vercel.app/">
-                <button className="card-button">View</button>
-              </a>
-            </div>
-          </Card>
-          <Card>
-            <div className="card-image-container">
-              <img
-                src="\assets\cloudAdventures.png"
-                alt="title"
-                className="card-image"
-              />
-            </div>
-            <div className="card-text-container">
-              <p className="card-title">Cloud Adventures</p>
-              <p className="description">
-                Cloud Adventures is a simple 3D dodging game with an
-                orthographic camera.
-              </p>
-              <a href="https://cloud-adventures.vercel.app/">
-                <button className="card-button">View</button>
-              </a>
-            </div>
-          </Card>
+          <div className="card-content">
+            <Card>
+              <div className="card-image-container">
+                <img
+                  src="\assets\playground.png"
+                  alt="title"
+                  className="card-image"
+                />
+              </div>
+              <div className="card-text-container">
+                <p className="card-title">Toby's Playground</p>
+                <p className="description">
+                  Toby's Playground is a fun and interactive web app designed
+                  for toddlers.
+                </p>
+                <a href="https://playground-toby.vercel.app/">
+                  <button className="card-button">View</button>
+                </a>
+              </div>
+            </Card>
+            <Card>
+              <div className="card-image-container">
+                <img
+                  src="\assets\cloudAdventures.png"
+                  alt="title"
+                  className="card-image"
+                />
+              </div>
+              <div className="card-text-container">
+                <p className="card-title">Cloud Adventures</p>
+                <p className="description">
+                  Cloud Adventures is a simple 3D dodging game with an
+                  orthographic camera.
+                </p>
+                <a href="https://cloud-adventures.vercel.app/">
+                  <button className="card-button">View</button>
+                </a>
+              </div>
+            </Card>
+            <Card>
+              <div className="card-image-container">
+                <img
+                  src="\assets\housetop.png"
+                  alt="title"
+                  className="card-image"
+                />
+              </div>
+              <div className="card-text-container">
+                <p className="card-title">Housetop Samgyupsal</p>
+                <p className="description">
+                  A Simple website I created for a client.
+                </p>
+                <a href="https://housetop-samgyupsal.vercel.app/">
+                  <button className="card-button">View</button>
+                </a>
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
